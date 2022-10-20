c257a52b8418fd53d084ec2102fe6aac8e07cf80
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2022-10-20 12:18:13 +0300

    Update models.py

--- apps/gallery_app/models.py
+++ apps/gallery_app/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Category model - create a table of category object
 class Category(models.Model):
+    # Each user have is own categories
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=False, blank=False)
     
     def __str__(self):
         return self.name
 
+# Photo model - create a table of photo object
 class Photo(models.Model):
+    # Category is a foreign key, so each user have is own categories and photos
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(null=False, blank=False)
     discription = models.TextField()
